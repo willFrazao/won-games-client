3727b14907c933443e1e9fb96236321b2873363c
william <william.frazao91@gmail.com>
2025-03-06 09:53:05 -0300

    first commit: configurando Themes e Fonts geral do projeto

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
+import theme from '../src/styles/theme'
+import React from 'react'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
