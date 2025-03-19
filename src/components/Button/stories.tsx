597c40417eb7e732c3c415ad35833da216886966
william <william.frazao91@gmail.com>
2025-03-19 09:44:35 -0300

    criando o componente Button

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import Button from '.'
+
+const meta: Meta<typeof Button> = {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {}
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {
+  args: {
+    children: 'Buy now'
+  }
+}
+
+export const withIcon: Story = {
+  args: {
+    size: 'small',
+    children: 'Buy now',
+    icon: <AddShoppingCart />
+  }
+}
