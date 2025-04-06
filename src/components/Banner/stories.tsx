9b6aa0894cf4c1d4948bcfb7c9d659450d913925
william <william.frazao91@gmail.com>
2025-04-06 10:28:46 -0300

    criando elemento de destaque Banner

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Banner, { BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta<typeof Banner>
+
+export const Default: StoryObj<BannerProps> = {}
